78aac8ba41eda6f8ca43e59cc767af7be42c2059
bq <email>
2021-05-13 14:42:36 +0800

    init

---
+++ src/main/java/com/github/makewheels/testspringbootmongo/TestSpringbootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.testspringbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringbootMongoApplication.class, args);
+    }
+
+}
